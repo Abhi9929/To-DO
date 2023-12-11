99ba5c1e9da73e7c838e1cf9454307ac5cf2054d
Abhishek Sharma <sharmaabhishek9929@gmail.com>
2023-12-11 15:07:03 +0530

    changes in file

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const taskList = document.getElementById("task-list");
-const inputTask = document.getElementById("task_box");
+const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("task_box");
 const addBtn = document.getElementById("add_btn");
 
-  let h3IdCounter = 1;  
-addBtn.addEventListener("click", () => {
-  let newTask = inputTask.value.toUpperCase();
 
+(function addTask() {
 
+  addBtn.addEventListener("click", () => {
+    let newTask = inputBox.value.toUpperCase();
   if (newTask == "") {
     window.alert("Please Add a Task!!");
   } else {
-    taskList.innerHTML += `
-        <div class="task_box">
-        <div class="tasks">
-        <h3 id="${1}">${newTask}</h3>
-        <button class="remove_btn"><i class="fa-solid fa-trash"></i></button>
-        </div>
-        </div>
-        `;
+    const li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
-
-
-  const trashButtons = document.querySelectorAll(".remove_btn");
-
-  trashButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      button.parentNode.parentNode.remove();
-    });
-  });
+  inputBox.value = "";
+  inputBox.focus();
+  saveData();
 });
+})();
 
+(function reviewTask() {
+  listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentNode.remove();
+      saveData();
+    }
+  });
+})();
+
+// To save our data on browser:
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+(function showList() {
+  listContainer.innerHTML = localStorage.getItem("data");
+})();
